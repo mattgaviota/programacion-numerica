86b362a3fc34f1a9cfaeb8f7bb502e0f1c718cb1
mattgaviota <mattgaviota@gmail.com>
2011-09-01 01:20:43 -0300

    agregado regula falsi y secante

--- raices/biseccion.py
+++ raices/biseccion.py
@@ -3,26 +3,44 @@
 from math import exp, fabs
 
 def f(x):
-    return exp(x)-(x)**2+4
+    return (x)**3-(x)-1
 
 def biseccion(a,b,e):
     alfa = a + (b-a)/2.0
-    if fabs(f(alfa)) < e and (a+b)/2.0 < e:
-        return alfa
-    else:
+    while fabs(f(alfa)) > e and (b-a)/2.0 > e:
         if f(a)*f(alfa) < 0:
-            return biseccion(a,alfa,e)
+            b = alfa
         else:
-            return biseccion(alfa,b,e)
+            a = alfa
+        alfa = a + (b-a)/2.0
+    return alfa
 
+def regula_falsi(a,b,e):
+    alfa = (a * f(b) - b * f(a))/ (f(b) - f(a))
+    while fabs(f(alfa)) > e and (b-a)/2.0 > e:
+        if f(a)*f(alfa) < 0:
+            b = alfa
+        else:
+            a = alfa
+        alfa = ((a * f(b)) - ( b * f(a)))/ (f(b) - f(a))
+    return alfa
 
-
+def secante(a,b,e):
+    alfa = b - (f(b) * ((b - a) / (f(b) - f(a))))
+    while fabs(f(alfa)) > e:
+        a = b
+        b = alfa
+        alfa = ((a * f(b)) - ( b * f(a)))/ (f(b) - f(a))
+    return alfa
+    
 def main():
 	
     a = raw_input('ingrese a: ')
     b = raw_input('ingrese b: ')
     e = raw_input('ingrese e: ')
-    print biseccion(float(a),float(b),float(e))
-
+    print 'biseccion :', biseccion(float(a),float(b),float(e))
+    print 'regula falsi : ', regula_falsi(float(a), float(b), float(e))
+    print 'secante : ', secante(float(a), float(b), float(e))
+    
 if __name__ == '__main__':
 	main()
